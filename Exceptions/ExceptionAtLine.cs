b74a1cfa24e4e5ac6b6da4707af25f22c3fe41ff
FeudeyTF <yanmordanenko089@gmail.com>
2024-12-07 01:24:28 +0300

    Added new Exception class

---
+++ Exceptions/ExceptionAtLine.cs
@@ -0,0 +1,15 @@
+﻿namespace FeuDumScript.Exceptions
+{
+    internal class ExceptionAtLine : Exception
+    {
+        public string Exception;
+
+        public int Position;
+
+        public ExceptionAtLine(string exception, int position) : base(exception + " Line: " + position)
+        {
+            Exception = exception;
+            Position = position;
+        }
+    }
+}
